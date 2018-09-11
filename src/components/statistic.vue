31af2e763b9a51e672eb7a1c8b4b9b395b0f7692
陈淑娴 <chenshuxian@huayugroup360.com>
2018-09-11 10:56:18 +0800

    csx

--- src/components/statistic.vue
+++ src/components/statistic.vue
@@ -1,9 +1,24 @@
 <template>
-  <div>
+  <div class="statistic">
     https://viserjs.github.io/docs.html#/viser/guide/installation
   </div>
 </template>
 
 <script>
-  
+	export default {
+		name: 'diaryList',
+		data() {
+			return {
+			}
+		}
+	}
 </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+	.statistic{
+		width: 1200px;
+		margin: 0 auto;
+		border: 1px solid #eee;
+	}
+</style>
